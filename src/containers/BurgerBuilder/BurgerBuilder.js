49a711ba81c60c1e30411e17592e61fbaee46e18
Evhenii <1godismyname1@gmail.com>
2018-10-23 15:26:46 +0300

    next step form

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,30 +88,18 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-      /*   //alert('You Continue');
-        this.setState({loading:true})
-
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Evhenii Drahancha',
-                adress: {
-                    street: 'Test Street 1',
-                    zipCode: '32113',
-                    country: 'Ukraine'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for(let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing:false})
-        })
-        .catch(error => {
-            this.setState({loading: false, purchasing:false})
-        }); */
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+
+        console.log(this.props.history)
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
 
